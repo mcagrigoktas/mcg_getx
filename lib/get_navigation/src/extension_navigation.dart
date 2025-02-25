3297da482eb1e3d6a80433c9f7dbe9e905f2751d
cagrigoktas <cagrigoktas@gmail.com>
2025-02-25 11:36:00 +0300

    Snackbar gosterimi silindi

--- lib/get_navigation/src/extension_navigation.dart
+++ lib/get_navigation/src/extension_navigation.dart
@@ -255,207 +255,6 @@ extension ExtensionDialog on GetInterface {
   }
 }
 
-extension ExtensionSnackbar on GetInterface {
-  SnackbarController rawSnackbar({
-    String? title,
-    String? message,
-    Widget? titleText,
-    Widget? messageText,
-    Widget? icon,
-    bool instantInit = true,
-    bool shouldIconPulse = true,
-    double? maxWidth,
-    EdgeInsets margin = const EdgeInsets.all(0.0),
-    EdgeInsets padding = const EdgeInsets.all(16),
-    double borderRadius = 0.0,
-    Color? borderColor,
-    double borderWidth = 1.0,
-    Color backgroundColor = const Color(0xFF303030),
-    Color? leftBarIndicatorColor,
-    List<BoxShadow>? boxShadows,
-    Gradient? backgroundGradient,
-    Widget? mainButton,
-    OnTap? onTap,
-    Duration? duration = const Duration(seconds: 3),
-    bool isDismissible = true,
-    DismissDirection? dismissDirection,
-    bool showProgressIndicator = false,
-    AnimationController? progressIndicatorController,
-    Color? progressIndicatorBackgroundColor,
-    Animation<Color>? progressIndicatorValueColor,
-    SnackPosition snackPosition = SnackPosition.BOTTOM,
-    SnackStyle snackStyle = SnackStyle.FLOATING,
-    Curve forwardAnimationCurve = Curves.easeOutCirc,
-    Curve reverseAnimationCurve = Curves.easeOutCirc,
-    Duration animationDuration = const Duration(seconds: 1),
-    SnackbarStatusCallback? snackbarStatus,
-    double barBlur = 0.0,
-    double overlayBlur = 0.0,
-    Color? overlayColor,
-    Form? userInputForm,
-  }) {
-    final getSnackBar = GetSnackBar(
-      snackbarStatus: snackbarStatus,
-      title: title,
-      message: message,
-      titleText: titleText,
-      messageText: messageText,
-      snackPosition: snackPosition,
-      borderRadius: borderRadius,
-      margin: margin,
-      duration: duration,
-      barBlur: barBlur,
-      backgroundColor: backgroundColor,
-      icon: icon,
-      shouldIconPulse: shouldIconPulse,
-      maxWidth: maxWidth,
-      padding: padding,
-      borderColor: borderColor,
-      borderWidth: borderWidth,
-      leftBarIndicatorColor: leftBarIndicatorColor,
-      boxShadows: boxShadows,
-      backgroundGradient: backgroundGradient,
-      mainButton: mainButton,
-      onTap: onTap,
-      isDismissible: isDismissible,
-      dismissDirection: dismissDirection,
-      showProgressIndicator: showProgressIndicator,
-      progressIndicatorController: progressIndicatorController,
-      progressIndicatorBackgroundColor: progressIndicatorBackgroundColor,
-      progressIndicatorValueColor: progressIndicatorValueColor,
-      snackStyle: snackStyle,
-      forwardAnimationCurve: forwardAnimationCurve,
-      reverseAnimationCurve: reverseAnimationCurve,
-      animationDuration: animationDuration,
-      overlayBlur: overlayBlur,
-      overlayColor: overlayColor,
-      userInputForm: userInputForm,
-    );
-
-    final controller = SnackbarController(getSnackBar);
-
-    if (instantInit) {
-      controller.show();
-    } else {
-      ambiguate(SchedulerBinding.instance)?.addPostFrameCallback((_) {
-        controller.show();
-      });
-    }
-    return controller;
-  }
-
-  SnackbarController showSnackbar(GetSnackBar snackbar) {
-    final controller = SnackbarController(snackbar);
-    controller.show();
-    return controller;
-  }
-
-  SnackbarController snackbar(
-    String title,
-    String message, {
-    Color? colorText,
-    Duration? duration = const Duration(seconds: 3),
-
-    /// with instantInit = false you can put snackbar on initState
-    bool instantInit = true,
-    SnackPosition? snackPosition,
-    Widget? titleText,
-    Widget? messageText,
-    Widget? icon,
-    bool? shouldIconPulse,
-    double? maxWidth,
-    EdgeInsets? margin,
-    EdgeInsets? padding,
-    double? borderRadius,
-    Color? borderColor,
-    double? borderWidth,
-    Color? backgroundColor,
-    Color? leftBarIndicatorColor,
-    List<BoxShadow>? boxShadows,
-    Gradient? backgroundGradient,
-    TextButton? mainButton,
-    OnTap? onTap,
-    bool? isDismissible,
-    bool? showProgressIndicator,
-    DismissDirection? dismissDirection,
-    AnimationController? progressIndicatorController,
-    Color? progressIndicatorBackgroundColor,
-    Animation<Color>? progressIndicatorValueColor,
-    SnackStyle? snackStyle,
-    Curve? forwardAnimationCurve,
-    Curve? reverseAnimationCurve,
-    Duration? animationDuration,
-    double? barBlur,
-    double? overlayBlur,
-    SnackbarStatusCallback? snackbarStatus,
-    Color? overlayColor,
-    Form? userInputForm,
-  }) {
-    final getSnackBar = GetSnackBar(
-        snackbarStatus: snackbarStatus,
-        titleText: titleText ??
-            Text(
-              title,
-              style: TextStyle(
-                color: colorText ?? iconColor ?? Colors.black,
-                fontWeight: FontWeight.w800,
-                fontSize: 16,
-              ),
-            ),
-        messageText: messageText ??
-            Text(
-              message,
-              style: TextStyle(
-                color: colorText ?? iconColor ?? Colors.black,
-                fontWeight: FontWeight.w300,
-                fontSize: 14,
-              ),
-            ),
-        snackPosition: snackPosition ?? SnackPosition.TOP,
-        borderRadius: borderRadius ?? 15,
-        margin: margin ?? const EdgeInsets.symmetric(horizontal: 10),
-        duration: duration,
-        barBlur: barBlur ?? 7.0,
-        backgroundColor: backgroundColor ?? Colors.grey.withOpacity(0.2),
-        icon: icon,
-        shouldIconPulse: shouldIconPulse ?? true,
-        maxWidth: maxWidth,
-        padding: padding ?? const EdgeInsets.all(16),
-        borderColor: borderColor,
-        borderWidth: borderWidth,
-        leftBarIndicatorColor: leftBarIndicatorColor,
-        boxShadows: boxShadows,
-        backgroundGradient: backgroundGradient,
-        mainButton: mainButton,
-        onTap: onTap,
-        isDismissible: isDismissible ?? true,
-        dismissDirection: dismissDirection,
-        showProgressIndicator: showProgressIndicator ?? false,
-        progressIndicatorController: progressIndicatorController,
-        progressIndicatorBackgroundColor: progressIndicatorBackgroundColor,
-        progressIndicatorValueColor: progressIndicatorValueColor,
-        snackStyle: snackStyle ?? SnackStyle.FLOATING,
-        forwardAnimationCurve: forwardAnimationCurve ?? Curves.easeOutCirc,
-        reverseAnimationCurve: reverseAnimationCurve ?? Curves.easeOutCirc,
-        animationDuration: animationDuration ?? const Duration(seconds: 1),
-        overlayBlur: overlayBlur ?? 0.0,
-        overlayColor: overlayColor ?? Colors.transparent,
-        userInputForm: userInputForm);
-
-    final controller = SnackbarController(getSnackBar);
-
-    if (instantInit) {
-      controller.show();
-    } else {
-      //routing.isSnackbar = true;
-      ambiguate(SchedulerBinding.instance)?.addPostFrameCallback((_) {
-        controller.show();
-      });
-    }
-    return controller;
-  }
-}
-
 extension GetNavigation on GetInterface {
   /// **Navigation.push()** shortcut.<br><br>
   ///
@@ -773,10 +572,10 @@ you can only use widgets and widget functions here''';
   }
 
   /// Returns true if a Snackbar, Dialog or BottomSheet is currently OPEN
-  bool get isOverlaysOpen => (isSnackbarOpen || isDialogOpen! || isBottomSheetOpen!);
+  bool get isOverlaysOpen => (isDialogOpen! || isBottomSheetOpen!);
 
   /// Returns true if there is no Snackbar, Dialog or BottomSheet open
-  bool get isOverlaysClosed => (!isSnackbarOpen && !isDialogOpen! && !isBottomSheetOpen!);
+  bool get isOverlaysClosed => (!isDialogOpen! && !isBottomSheetOpen!);
 
   /// **Navigation.popUntil()** shortcut.<br><br>
   ///
@@ -798,17 +597,8 @@ you can only use widgets and widget functions here''';
   }) {
     //TODO: This code brings compatibility of the new snackbar with GetX 4,
     // remove this code in version 5
-    if (isSnackbarOpen && !closeOverlays) {
-      closeCurrentSnackbar();
-      return;
-    }
 
     if (closeOverlays && isOverlaysOpen) {
-      //TODO: This code brings compatibility of the new snackbar with GetX 4,
-      // remove this code in version 5
-      if (isSnackbarOpen) {
-        closeAllSnackbars();
-      }
       navigator?.popUntil((route) {
         return (!isDialogOpen! && !isBottomSheetOpen!);
       });
@@ -1079,17 +869,6 @@ you can only use widgets and widget functions here''';
 
   /// give name from previous route
   String get previousRoute => routing.previous;
-
-  /// check if snackbar is open
-  bool get isSnackbarOpen => SnackbarController.isSnackbarBeingShown; //routing.isSnackbar;
-
-  void closeAllSnackbars() {
-    SnackbarController.cancelAllSnackbars();
-  }
-
-  Future<void> closeCurrentSnackbar() async {
-    await SnackbarController.closeCurrentSnackbar();
-  }
 
   /// check if dialog is open
   bool? get isDialogOpen => routing.isDialog;
--- lib/get_utils/get_utils.dart
+++ lib/get_utils/get_utils.dart
@@ -1,4 +1,3 @@
 export 'src/extensions/export.dart';
 export 'src/get_utils/get_utils.dart';
 export 'src/platform/platform.dart';
-export 'src/queue/get_queue.dart';

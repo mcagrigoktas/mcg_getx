4f39fcb186a382dc77218e97760976d94caba756
cagrigoktas <cagrigoktas@gmail.com>
2025-08-15 23:47:13 +0300

    feat: Gereksiz GetSingleTickerProviderStateMixin ve ilgili kod kaldırıldı

--- lib/get_state_manager/get_state_manager.dart
+++ lib/get_state_manager/get_state_manager.dart
@@ -3,7 +3,6 @@ library get_state_manager;
 export 'src/rx_flutter/rx_disposable.dart';
 export 'src/rx_flutter/rx_notifier.dart';
 export 'src/rx_flutter/rx_obx_widget.dart';
-export 'src/rx_flutter/rx_ticket_provider_mixin.dart';
 export 'src/simple/get_controllers.dart';
 export 'src/simple/get_responsive.dart';
 export 'src/simple/get_state.dart';
